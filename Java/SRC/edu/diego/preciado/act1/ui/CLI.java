a24cc347a3747f478fe922054376ac76cd4268b1
Diego Preciado codes <preciadodiego258@gmail.com>
2026-01-14 17:04:57 -0600

    primer codigo.

--- Java/SRC/edu/diego/preciado/act1/ui/CLI.java
+++ Java/SRC/edu/diego/preciado/act1/ui/CLI.java
@@ -1,19 +1,19 @@
 package edu.diego.preciado.act1.ui;
 
-import edu.diego.preciado.act1.proces.Switch;{
+import edu.diego.preciado.act1.proces.Switch;
 
 public class CLI{
     public static void start(){
-            Switch switch_wifi=new Switch();
-            System.out.println("Bienvenido al programa");
-            System.out.println("El switch est"+switch_wifi.isOn();
-            if(switch_wifi.isOn()){
-                System.out.println("dese apagar el switch");
-            }
-            else{
-                System.out.println("desea prender el switch");
-            }
-            System.out.println("el swotch esta"+switch_wifi.isOn ();}
+        Switch switch_wifi=new Switch();
+        System.out.println("bienvenido al programa");
+        System.out.println("el switch esta"+switch_wifi.isOn());
+        if(switch_wifi.isOn()){
+            System.out.println("desea apagar el switch");
+        }
+        else{
+            System.out.println("desea prender el switch");
+        }
+        System.out.println("el switch esta"+switch_wifi.isOn());
     }
 
 }
