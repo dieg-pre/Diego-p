a647ce477bc4acb059cbbcd250cd4f806bab9a10
Diego Preciado codes <preciadodiego258@gmail.com>
2026-01-14 18:22:44 -0600

    segundo codigo diego p

--- Java/SRC/edu/diego/preciado/act1/ui/CLI.java
+++ Java/SRC/edu/diego/preciado/act1/ui/CLI.java
@@ -7,11 +7,15 @@ public class CLI{
         Switch switch_wifi=new Switch();
         System.out.println("bienvenido al programa");
         System.out.println("el switch esta"+switch_wifi.isOn());
-        if(switch_wifi.isOn()){
-            System.out.println("desea apagar el switch");
-        }
-        else{
+        String respuesta;
+        if(switch_wifi.isOn()) {
+            System.out.println("desea apagar el switch: s/n");
+            respuesta = scanner.nextLine();
+            if (respuesta.equals("n")) switch_wifi.prenderSwitch();
+        }else  {
             System.out.println("desea prender el switch");
+            respuesta = scanner.nextLine();
+            if(respuesta.equals("s")) switch_wifi.prenderSwitch();
         }
         System.out.println("el switch esta"+switch_wifi.isOn());
     }
